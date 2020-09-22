2cc35d9199554dc5665ed29c0c92814ff294d328
Evan Pon <evanpon@gmail.com>
2020-09-21 21:07:14 -0700

    connect to firestore.

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,6 @@
 import 'package:flutter/material.dart';
+import 'package:firebase_core/firebase_core.dart';
+import 'package:cloud_firestore/cloud_firestore.dart';
 
 void main() {
   runApp(MyApp());
@@ -8,14 +10,27 @@ class MyApp extends StatelessWidget {
   // This widget is the root of your application.
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(
-      title: 'Flutter Demo',
-      theme: ThemeData(
-        primarySwatch: Colors.blue,
-        visualDensity: VisualDensity.adaptivePlatformDensity,
-      ),
-      home: MyHomePage(title: 'Toad'),
-    );
+    return FutureBuilder(
+        // Initialize FlutterFire:
+        future: Firebase.initializeApp(),
+        builder: (context, snapshot) {
+          // Check for errors
+          if (snapshot.hasError) {
+            return const Text("Error. Missing GoogleService-Info.plist file?",
+                textDirection: TextDirection.ltr);
+          }
+          if (snapshot.connectionState == ConnectionState.done) {
+            return MaterialApp(
+              title: 'Flutter Demo',
+              theme: ThemeData(
+                primarySwatch: Colors.blue,
+                visualDensity: VisualDensity.adaptivePlatformDensity,
+              ),
+              home: MyHomePage(title: 'Flutter Demo Home Page'),
+            );
+          }
+          return const Text("Loading", textDirection: TextDirection.ltr);
+        });
   }
 }
 
@@ -35,16 +50,27 @@ class _MyHomePageState extends State<MyHomePage> {
       appBar: AppBar(
         title: Text(widget.title),
       ),
-      body: Center(
-        child: Column(
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: <Widget>[
-            Text(
-              'You have pushed the button this many times:',
-            ),
-          ],
-        ),
-      ),
+      body: StreamBuilder(
+          stream: FirebaseFirestore.instance
+              .collection('users')
+              .where("name", isEqualTo: "Evan")
+              .snapshots(),
+          builder: (context, snapshot) {
+            if (!snapshot.hasData) return const Text('Loading');
+
+            return ListView.builder(
+                itemExtent: 80,
+                itemCount: snapshot.data.documents.length,
+                itemBuilder: (context, index) => ListTile(
+                        title: Row(
+                      children: [
+                        Expanded(
+                          child: Text(
+                              snapshot.data.documents[index].data().toString()),
+                        )
+                      ],
+                    )));
+          }),
     );
   }
 }
